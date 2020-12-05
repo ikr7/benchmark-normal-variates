5d99a8c31bc29ca007fe0ae74800d651f4df9d21
ikr7 <kunpei.ikuta@gmail.com>
2020-12-06 02:45:56 +0900

    milliseconds!

--- generate-table.py
+++ generate-table.py
@@ -6,7 +6,7 @@ output_path = Path('tex/benchmark-result.tex')
 output_text = ''
 output_text += '\\begin{tabular}{crrrr}\n'
 output_text += '    \\toprule\n'
-output_text += '    アルゴリズム & 実行時間 ($\\mu$s) & 速度 \\tnote{a} & $N_U$ \\tnote{b} \\\\\n'
+output_text += '    アルゴリズム & 実行時間 (ms) & 速度 \\tnote{a} & $N_U$ \\tnote{b} \\\\\n'
 output_text += '    \\midrule\n'
 
 for line in input_path.read_text().split('\n')[1:-1]:
@@ -14,7 +14,7 @@ for line in input_path.read_text().split('\n')[1:-1]:
     time = float(time)
     rate = float(rate)
     samples = float(samples)
-    output_text += f'    {algorithm} & {time :.0f} & {rate :.0f} & {samples :.4f} \\\\\n'
+    output_text += f'    {algorithm} & {time / 1000:.0f} & {rate :.0f} & {samples :.4f} \\\\\n'
 
 output_text += '    \\bottomrule\n'
 output_text += '\\end{tabular}\n'
